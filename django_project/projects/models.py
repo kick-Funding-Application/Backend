0dfb5e3326f6cd754453ca48e74b63820a11f2c2
Mohamed-A-Badr <badr49960@gmail.com>
2023-04-16 16:46:24 +0200

    Add category field to Project model

--- django_project/projects/models.py
+++ django_project/projects/models.py
@@ -3,11 +3,23 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    CATEGORY_CHOICES = [
+        ("health", "Health"),
+        ("education", "Education"),
+        ("environment", "Environment"),
+        ("animal", "Animal"),
+        ("culture & art", "Culture & Art"),
+        ("other", "Other"),
+    ]
+
+    title = models.CharField(max_length=150, unique=True)
     details = models.TextField()
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
     created_dt = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(
+        max_length=50, choices=CATEGORY_CHOICES, default="other"
+    )
     tags = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
@@ -15,7 +27,7 @@ class Project(models.Model):
 
 
 class Thumbnail(models.Model):
-    image_url = models.CharField(max_length=150)
+    image_url = models.CharField(max_length=255)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
     def __str__(self):
