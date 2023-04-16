a2076562ba9fd053f684713437c50af9ab21d0b8
Mohamed-A-Badr <badr49960@gmail.com>
2023-04-16 15:11:53 +0200

    Add Project model

--- django_project/projects/models.py
+++ django_project/projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    details = models.TextField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    created_dt = models.DateTimeField(auto_now_add=True)
+    tags = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
