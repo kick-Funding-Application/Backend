6b870287144412ab1cdf071e7e1e6e8d4e06decb
Mohamed-A-Badr <badr49960@gmail.com>
2023-05-01 01:33:46 +0300

    Adding Filter by category route

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
 from .serializers import ProjectSerializer, ThumbnailSerializer
 from projects.models import Project, Thumbnail
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -13,3 +14,15 @@ class ProjectViewSets(viewsets.ModelViewSet):
 class ThumbnailViewSets(viewsets.ModelViewSet):
     queryset = Thumbnail.objects.all()
     serializer_class = ThumbnailSerializer
+
+
+class ProjectByCatergoryAPI(generics.ListAPIView):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        try:
+            category = self.kwargs.get("category")
+            project = Project.objects.filter(category=category)
+            return project
+        except Project.DoesNotExist:
+            return Project.objects.none()
