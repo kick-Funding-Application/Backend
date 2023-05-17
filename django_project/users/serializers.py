82e4305d88032cd0ea23f16f55436ac316255377
Mohamed-A-Badr <badr49960@gmail.com>
2023-05-17 23:01:52 +0300

    update user serializers

--- django_project/users/serializers.py
+++ django_project/users/serializers.py
@@ -4,37 +4,38 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 from django_countries.serializer_fields import CountryField
 from django.conf import settings
 import datetime
+
 User = settings.AUTH_USER_MODEL
 
 
 class CustomRegisterSerializer(RegisterSerializer):
-    username = serializers.CharField(max_length=150,read_only=True)
+    username = serializers.CharField(max_length=150, read_only=True)
     first_name = serializers.CharField(max_length=20)
     last_name = serializers.CharField(max_length=20)
     phone_number = serializers.IntegerField()
     country = CountryField()
-    user_image = serializers.ImageField(required=False)
+    user_image = serializers.CharField(required=False)
     birth_date = serializers.DateField()
-    def custom_signup(self, request, user):
-        user.username = self.validated_data['username']
-        user.first_name = self.validated_data['first_name']
-        user.last_name = self.validated_data['last_name']
-        user.phone_number = self.validated_data['phone_number']
-        user.country = self.validated_data.get('country')
-        user.user_image = self.validated_data.get("user_image")
-        user.birth_date = self.validated_data['birth_date']
 
+    def custom_signup(self, request, user):
+        user.username = self.validated_data["username"]
+        user.first_name = self.validated_data["first_name"]
+        user.last_name = self.validated_data["last_name"]
+        user.phone_number = self.validated_data["phone_number"]
+        user.country = self.validated_data.get("country")
+        user.user_image = self.validated_data.get("user_image")
+        user.birth_date = self.validated_data["birth_date"]
 
         user.save()
 
 
 class CustomUserDetailsSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=150,read_only=True)
+    username = serializers.CharField(max_length=150, read_only=True)
     first_name = serializers.CharField(max_length=20)
     last_name = serializers.CharField(max_length=20)
     phone_number = serializers.IntegerField()
     country = CountryField()
-    user_image = serializers.ImageField()
+    user_image = serializers.CharField()
     birth_date = serializers.DateField()
 
     def update(self, instance, validated_data):
